24537d16facad2ad7123a931b320a6f1c678f217
Sabrina <sabrinawilliamsde@gmail.comgit config --global color.ui autogit config --get user.namegit config --global user.email sabrinawilliamsde@gmail.com>
2024-02-07 12:45:08 +0100

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    let newString = '';
+    for (let i = 1; i <= stringArray.length; i++) {
+    newString += stringArray[stringArray.length - i];
+}   
+    return newString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
