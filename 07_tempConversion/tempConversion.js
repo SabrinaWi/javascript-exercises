6e035cf2ca8a4488d699b4d38202ef4713f8db89
Sabrina <sabrinawilliamsde@gmail.comgit config --global color.ui autogit config --get user.namegit config --global user.email sabrinawilliamsde@gmail.com>
2024-02-19 10:49:18 +0100

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC;
+  //converts from Fahrenheit to Celcius
+  tempC = (tempF - 32) * 5/9;
+  //rounds to 1 decimal and returns value
+  return Math.round(tempC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF;
+  //converts from Celcius to Fahrenheit
+  tempF = (tempC * 9/5 + 32);
+  //rounds to 1 decimal and returns value
+  return Math.round(tempF * 10) / 10;
 };
 
 // Do not edit below this line
