0ad5eea6768f5ecc844ec9de73c8e1101eeda70b
Sabrina <sabrinawilliamsde@gmail.comgit config --global color.ui autogit config --get user.namegit config --global user.email sabrinawilliamsde@gmail.com>
2024-08-20 14:08:53 +0200

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, num) => acc + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, num) => acc * num);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1; // Initialize a result variable to 1
+  for (let i = 1; i <= num; i++) {
+    // Loop from 1 to num
+    result *= i; // Multiply result by the current number i
+  }
+  return result; // Return the final result
 };
+
+console.log(factorial(5)); // Outputs: 120
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
