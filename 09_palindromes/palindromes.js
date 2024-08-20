a86f554d16c9272410a4805cf4ee398169d602ab
Sabrina <sabrinawilliamsde@gmail.comgit config --global color.ui autogit config --get user.namegit config --global user.email sabrinawilliamsde@gmail.com>
2024-08-20 15:32:36 +0200

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let arr = string.split("").filter(function (char) {
+    return (
+      ("a" <= char && char <= "z") ||
+      ("A" <= char && char <= "Z") ||
+      ("0" <= char && char <= "9")
+    );
+  });
+  let arrOriginal = [...arr];
+  let arrReversed = arr.reverse();
+  return (
+    arrOriginal.join("").toLowerCase() === arrReversed.join("").toLowerCase()
+  );
 };
 
 // Do not edit below this line
