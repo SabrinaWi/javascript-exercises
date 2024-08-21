137e012f7d2cd3f782650d7416b4aac89c2e7142
Sabrina <sabrinawilliamsde@gmail.comgit config --global color.ui autogit config --get user.namegit config --global user.email sabrinawilliamsde@gmail.com>
2024-08-21 12:05:24 +0200

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+  if (+a == 0) {
+    return 0;
+  } else if (+a < 0) {
+    return "OOPS";
+  } else {
+    let arr = [1, 1];
+    for (let i = 2; i < +a; i++) {
+      arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr[+a - 1];
+  }
 };
 
 // Do not edit below this line
